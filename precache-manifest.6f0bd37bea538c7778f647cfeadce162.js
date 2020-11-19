ee3a2608ebdf462861c0424f775f3a27c43c40bf
Pablo Rivas Camino UDC <pablo.rivas.camino@udc.es>
2020-11-19 18:34:56 +0100

    Updates

---
+++ precache-manifest.6f0bd37bea538c7778f647cfeadce162.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09515cb5d302a1787ad0a45930151862",
+    "url": "/femmefeten/index.html"
+  },
+  {
+    "revision": "1c83b6ada7352fefd492",
+    "url": "/femmefeten/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "29dee086bc44304b06d5",
+    "url": "/femmefeten/static/css/main.d91f51c1.chunk.css"
+  },
+  {
+    "revision": "1c83b6ada7352fefd492",
+    "url": "/femmefeten/static/js/2.b18a6e9e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/femmefeten/static/js/2.b18a6e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29dee086bc44304b06d5",
+    "url": "/femmefeten/static/js/main.90c86c09.chunk.js"
+  },
+  {
+    "revision": "2b3f14a3195a1027993d",
+    "url": "/femmefeten/static/js/runtime-main.c67fcb21.js"
+  },
+  {
+    "revision": "2fe27d9d10cdfccb1baef28a45d5ba90",
+    "url": "/femmefeten/static/media/Lato-Light.2fe27d9d.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/femmefeten/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b5c8c7d234cbe3e305e4d1791f5903c9",
+    "url": "/femmefeten/static/media/logo.b5c8c7d2.png"
+  },
+  {
+    "revision": "c65e05e05c07bc764b3b1c26b48b892a",
+    "url": "/femmefeten/static/media/logo2.c65e05e0.jpeg"
+  },
+  {
+    "revision": "e1422c04a632738c3c3bc3e3cf376e3c",
+    "url": "/femmefeten/static/media/sesion01.e1422c04.jpg"
+  },
+  {
+    "revision": "8ba099dd3d9b3da985ce992f8fcdd05c",
+    "url": "/femmefeten/static/media/sesion02.8ba099dd.jpg"
+  },
+  {
+    "revision": "9d2b9d470546a46d8c3c319f3ec58076",
+    "url": "/femmefeten/static/media/sesion03.9d2b9d47.jpg"
+  },
+  {
+    "revision": "222e08f6950909eb2e60103d99245542",
+    "url": "/femmefeten/static/media/sesion04.222e08f6.jpg"
+  },
+  {
+    "revision": "5e22fdb1572d671e2a8fca41be54f8b8",
+    "url": "/femmefeten/static/media/sesion05.5e22fdb1.jpg"
+  },
+  {
+    "revision": "005a6359f03e5946d7f56a5daf3e2581",
+    "url": "/femmefeten/static/media/video01.005a6359.MOV"
+  }
+]);
